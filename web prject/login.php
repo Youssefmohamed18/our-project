8ffffed2c10fc081ea0953e84a076ce264f0a02a
Youssefmohamed18 <110970240+Youssefmohamed18@users.noreply.github.com>
2024-12-21 10:10:27 +0200

    trendy

---
+++ web prject/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trendy Store</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+      
+   <div class="container" id="signup" >
+    <h1 class="form-title">Register</h1>
+    <form method="post" action="register.php">
+        <div class="input-group">
+            <i class="fas fa-user"></i>
+            <input type="text" name="fname" id="fname" placeholder="First Name" required>
+            <label for="fname">First Name</label>
+        </div>
+        <div class="input-group">
+            <i class="fas fa-user"></i>
+            <input type="text" name="lName" id="lname" placeholder="Last Name" required>
+            <label for="lName">Last Name</label>
+        </div>
+        <div class="input-group">
+            <i class="fas fa-envelope"></i>
+            <input type="email" name="email" id="email" placeholder="email" required>
+            <label for="email">Email</label>
+        </div>
+        <div class="input-group">
+            <i class="fas fa-lock"></i>
+            <input type="password" name="password" id="password" placeholder="password" required>
+            <label for="password">Password</label>
+        </div>
+        <input type="submit" class="bth" value="Sign Up" name="signup" onclick="window.location.href='project.html';">
+
+
+    </form>
+    
+   </div>
+    
+        
+        
+    
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
--- web prject/script.js
+++ web prject/script.js
@@ -1,6 +1,9 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+const signInButton = document.getElementById('signInButton');
+const signInForm = document.getElementById('signInForm');
+
 
 if (bar) {
     bar.addEventListener('click', () => {
